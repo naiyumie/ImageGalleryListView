546c3928f14193c37903d5b033877c7d7fb92550
naiyumie <naiyumie@gmail.com>
2021-02-11 13:33:10 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace ImageGalleryListView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listViewFile = new System.Windows.Forms.ListView();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listViewFile
+            // 
+            this.listViewFile.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewFile.Font = new System.Drawing.Font("바탕체", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.listViewFile.HideSelection = false;
+            this.listViewFile.Location = new System.Drawing.Point(6, 7);
+            this.listViewFile.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listViewFile.Name = "listViewFile";
+            this.listViewFile.Size = new System.Drawing.Size(963, 660);
+            this.listViewFile.TabIndex = 0;
+            this.listViewFile.UseCompatibleStateImageBehavior = false;
+            //this.listViewFile.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listViewFile_KeyDown);
+            this.listViewFile.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listViewFile_MouseDoubleClick);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Font = new System.Drawing.Font("바탕체", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(983, 704);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.listViewFile);
+            this.tabPage1.Location = new System.Drawing.Point(4, 26);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(975, 674);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "아케이드";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.pictureBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 26);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(975, 674);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "단축키";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(966, 662);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("궁서체", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "슈퍼소니코 게임센터";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewFile;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+
+
+namespace ImageGalleryListView
+{
+    public partial class Form1 : Form
+    {
+        /* variables 초기로딩 */
+        System.Windows.Forms.ImageList myImageListLarge = new ImageList();
+        int lstvf_add_count = 0;
+        XmlNodeList xmlList;
+        String DriveName = @"E:";
+        String BasicPath = @"E:\\_Emul_\\";
+        String SnapShotFolderName;
+        String AssetsFolderName;
+
+        public Form1()
+        {
+           
+            InitializeComponent();
+
+            /* variables 할당 */
+            SnapShotFolderName = BasicPath + @"__super_sonico_snap\\";
+            AssetsFolderName = BasicPath + @"__super_sonico_assets\\";
+
+            /* 이미지 리스트 세팅 */
+            listViewFile.Items.Clear();
+            listViewFile.LargeImageList = myImageListLarge;
+            myImageListLarge.ImageSize = new Size(255, 149);
+            listViewFile.View = View.LargeIcon;
+
+            /* XML 로드 */
+            string xmlprt = ""; 
+            XmlDocument xml = new XmlDocument(); 
+            xml.Load(AssetsFolderName + @"arcade.xml");
+            xmlList = xml.SelectNodes("/games/game");
+
+            foreach (XmlNode xnl in xmlList) {
+                xmlprt += xnl["name"].InnerText;
+                xmlprt += xnl["snap"].InnerText;
+                xmlprt += xnl["file"].InnerText;
+                xmlprt += xnl["emul"].InnerText;
+                xmlprt += xnl["roms"].InnerText;
+                xmlprt += xnl["exec"].InnerText;
+
+                String fullpathfile = SnapShotFolderName + @"\\" + xnl["snap"].InnerText;
+                FileInfo fileinfo = new FileInfo(fullpathfile);
+                //Console.WriteLine(fileinfo);
+
+                /* 이미지 할당 및 텍스트 실행경로등 할당 */
+                using (FileStream stream = new FileStream(fullpathfile, FileMode.Open, FileAccess.Read))
+                {
+                    myImageListLarge.Images.Add(Image.FromStream(stream));
+                    listViewFile.Items.Add(new ListViewItem
+                    {
+                        ImageIndex = lstvf_add_count,
+                        Text = xnl["name"].InnerText + Environment.NewLine
+                             + xnl["file"].InnerText
+                    });
+                }
+                lstvf_add_count++;
+            }
+
+
+            /* 단축키 이미지 */ 
+            pictureBox1.Image = Bitmap.FromFile(AssetsFolderName + @"shortcut.png");
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+
+        private void listViewFile_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            /* 선택된 아이템의 경로를 얻는다 */
+            ListView.SelectedListViewItemCollection items = listViewFile.SelectedItems;
+            ListViewItem lvItem = items[0];
+            //MessageBox.Show(lvItem.SubItems[0].Text);
+            String FullRunPath = BasicPath + xmlList[lvItem.Index]["emul"].InnerText + @"\\" + xmlList[lvItem.Index]["roms"].InnerText + @"\\" + xmlList[lvItem.Index]["file"].InnerText;
+            //MessageBox.Show(FullRunPath);
+
+            String FileNameOnly = xmlList[lvItem.Index]["file"].InnerText.Replace(".zip","");
+
+            String runcmd = AssetsFolderName + @"run_arcade.bat " + DriveName + 
+                @" " + BasicPath + xmlList[lvItem.Index]["emul"].InnerText + @"/ " + xmlList[lvItem.Index]["exec"].InnerText + 
+                @" " + FileNameOnly;
+
+            //MessageBox.Show(runcmd);
+            Console.WriteLine(runcmd);
+
+            /* CMD 실행 */
+            System.Diagnostics.ProcessStartInfo pri = new System.Diagnostics.ProcessStartInfo();
+            System.Diagnostics.Process pro = new System.Diagnostics.Process();
+
+            pri.FileName = "cmd.exe";
+            pri.CreateNoWindow = true;
+            pri.UseShellExecute = false;
+
+            pri.RedirectStandardInput = true;
+            pri.RedirectStandardOutput = true;
+            pri.RedirectStandardError = true;
+
+            pro.StartInfo = pri;
+            pro.Start();
+
+            pro.StandardInput.Write(runcmd + Environment.NewLine);
+            pro.StandardInput.Close();
+            string resultValue = pro.StandardOutput.ReadToEnd();
+            pro.WaitForExit();
+            pro.Close();
+
+            //MessageBox.Show(resultValue);
+            //Console.WriteLine(resultValue);
+
+        }
+
+    }
+}
